9671a3826509b5ad6cdc0da624d6fd4be990522b
Maxwell Haley <maxwell.haley@openmailbox.org>
2017-08-01 14:08:57 -0400

    Made changes based off recommendations from ESLint
    Tab in 'hours' array that extends multiple lines. Add semicolons where
    applicable. Switch double-quotes to single-quotes where applicable.
    Remove 'response' variable that was suppose to be removed in a previous
    commit.

--- talking_clock/talking_clock.js
+++ talking_clock/talking_clock.js
@@ -1,27 +1,25 @@
 const tod = ['am', 'pm'];
 const hours = ['twelve', 'one', 'two', 'three', 'four', 'five', 'six', 'seven',
-'eight', 'nine', 'ten', 'eleven', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
-'seventeen', 'eighteen', 'nineteen'];
+    'eight', 'nine', 'ten', 'eleven', 'thirteen', 'fourteen', 'fifteen',
+    'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 const minutes = ['oh', 'twenty', 'thirty', 'forty', 'fifty', 'sixty'];
 
 const validateUserTime = (usrTime) => {
     /* Validate return the status of testing param usrTime against
      * a valid regex. */
-    const validRegex = /^\d?\d:?\d{2}$/
+    const validRegex = /^\d?\d:?\d{2}$/;
     return validRegex.test(usrTime);
-}
+};
 
 const displayError = (usrTime, response) => {
     /* Display an error to the user, using their input param usrTime */
-    const response = document.getElementById('clockResponse');
-
-    if (usrTime === "") {
+    if (usrTime === '') {
         usrTime = 'Nothing';
     }
 
     response.classList.add('error');
     response.innerHTML = `"${usrTime}" is not a valid 24-hour time.`;
-}
+};
 
 const formatTime = (usrTime) => {
     /* Formats the time to always follow the format: ##:## */
@@ -37,7 +35,7 @@ const formatTime = (usrTime) => {
     }
 
     return usrTime;
-}
+};
 
 const displayTime = (usrTime) => {
     /* Format the param usrTime if necessary, then display to user in
@@ -46,9 +44,9 @@ const displayTime = (usrTime) => {
         usrTime = formatTime(usrTime);
     }
     // TODO: Convert usrTime (numerical) to English (alphabetical).
-}
+};
 
-document.getElementById("btnGetTime").onclick = () =>  {
+document.getElementById('btnGetTime').onclick = () =>  {
     /* Flow to validate user input, then display results.  */
     const response = document.getElementById('clockResponse');
 
@@ -57,11 +55,11 @@ document.getElementById("btnGetTime").onclick = () =>  {
         response.classList.remove('error');
     }
 
-    const usrTime = document.getElementById("txtTime").value;
+    const usrTime = document.getElementById('txtTime').value;
 
     if (validateUserTime(usrTime) === false) {
         displayError(usrTime, response);
     } else {
         displayTime(usrTime);
     }
-}
+};
